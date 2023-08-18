5c4d1584057dd71b280ada6fcf56cdceb4729e37
Katie Stevens Andersen <KAstev@gmail.com>
2023-08-17 20:05:12 -0600

    Make genre dummies

--- src/clean_data.py
+++ src/clean_data.py
@@ -10,14 +10,15 @@ def clean_data():
         .astype({"id": "object"})
         .assign(
             release_date=lambda x: pd.to_datetime(x["release_date"], errors="coerce"),
-            genres=lambda x: x.genres.apply(eval)
+            genres=lambda x: x.genres.apply(eval),
+            production_companies=lambda x: x.production_companies.apply(eval)
         )
         .query("vote_count > 0")
         .query("budget > 0")
         .query("revenue > 0")
         .set_index("id")
     )
-    
+
     return pd.concat(
         [df, pd.get_dummies(df.genres.explode()).groupby(level=0).sum()], 
         axis=1
