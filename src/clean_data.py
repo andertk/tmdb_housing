b3a946eaa2232924cc0b27bfeacd65397be9c0a9
Katie Stevens Andersen <KAstev@gmail.com>
2023-08-17 19:24:26 -0600

    Create genre dummies

--- src/clean_data.py
+++ src/clean_data.py
@@ -5,16 +5,23 @@ RAW_MOVIES_PATH = "data/raw/top_1000_popular_movies_tmdb.csv"
 
 
 def clean_data():
-    clean_data = (
+    df = (
         pd.read_csv(RAW_MOVIES_PATH, engine="python", index_col=0)
         .astype({"id": "object"})
-        .assign(release_date=lambda x: pd.to_datetime(x["release_date"], errors="coerce"))
+        .assign(
+            release_date=lambda x: pd.to_datetime(x["release_date"], errors="coerce"),
+            genres=lambda x: x.genres.apply(eval)
+        )
         .query("vote_count > 0")
         .query("budget > 0")
         .query("revenue > 0")
         .set_index("id")
     )
-    return clean_data
+    
+    return pd.concat(
+        [df, pd.get_dummies(df.genres.explode()).groupby(level=0).sum()], 
+        axis=1
+    )
 
 
 if __name__ == "__main__":
