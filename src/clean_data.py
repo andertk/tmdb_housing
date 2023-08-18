a6bbebfecc1acdcaa667abe09c33f2e9605dda3c
Travis Andersen <travisander7@gmail.com>
2023-08-17 20:15:01 -0600

    Added production companies to clean data

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,29 +1,30 @@
 import pandas as pd
 
 
-RAW_MOVIES_PATH = "data/raw/top_1000_popular_movies_tmdb.csv"
+def get_dummies_from_lists(x):
+    return (
+        x
+        .apply(eval)
+        .explode()
+        .pipe(pd.get_dummies)
+        .groupby("id")
+        .sum()
+        .clip(upper=1)
+    )
 
 
-def clean_data():
+def clean_data(raw_path):
     df = (
-        pd.read_csv(RAW_MOVIES_PATH, engine="python", index_col=0)
+        pd.read_csv(raw_path, engine="python", index_col=0)
         .astype({"id": "object"})
-        .assign(
-            release_date=lambda x: pd.to_datetime(x["release_date"], errors="coerce"),
-            genres=lambda x: x.genres.apply(eval)
-        )
+        .assign(release_date=lambda x: pd.to_datetime(x["release_date"], errors="coerce"))
         .query("vote_count > 0")
         .query("budget > 0")
         .query("revenue > 0")
         .set_index("id")
     )
-    
-    return pd.concat(
-        [df, pd.get_dummies(df.genres.explode()).groupby(level=0).sum()], 
-        axis=1
-    )
 
+    genres = get_dummies_from_lists(df["genres"])
+    production_companies = get_dummies_from_lists(df["production_companies"])
 
-if __name__ == "__main__":
-    clean_data = clean_data()
-    
+    return pd.concat([df, genres, production_companies], axis=1)
