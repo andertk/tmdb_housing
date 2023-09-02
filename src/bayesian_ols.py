201496a4f4facc3e76e60cf99b82e292afe0561a
Travis Andersen <travisander7@gmail.com>
2023-09-02 00:29:27 -0600

    Added predict method

--- src/bayesian_ols.py
+++ src/bayesian_ols.py
@@ -6,15 +6,17 @@ from tqdm import tqdm
 class BayesianOLS:
     def __init__(self, X, y, param_groups, add_intercept=True) -> None:
         self.X = X.copy()
-        if add_intercept:
-            self.X.insert(0, 'intercept', 1)
+        self.add_intercept = add_intercept
         self.y = y.copy()
         self.param_groups = param_groups
+
+        if add_intercept:
+            self.X.insert(0, 'intercept', 1)
 
     def likelihood(self, theta):
         beta = theta.drop('sigma2')
         sigma2 = theta.loc['sigma2']
-        return norm.logpdf(self.y, loc=self.X.mul(beta).sum(axis=1), scale=sigma2).sum()
+        return norm.logpdf(self.y, loc=self.X.dot(beta.T), scale=sigma2).sum()
 
     def prior(self, theta):
         return sum([
@@ -45,6 +47,18 @@ class BayesianOLS:
                 accept = log_ratio > uniform.rvs()
                 draws.loc[i, j["parameters"]] = prop.loc[j["parameters"]] if accept else prev.loc[j["parameters"]]
         self.draws = draws
+
+    def predict(self, X):
+        X = X.copy()
+        beta = self.draws.drop(columns='sigma2')
+        sigma2 = self.draws['sigma2']
+        if self.add_intercept:
+            X.insert(0, 'intercept', 1)
+        pred_df = pd.DataFrame(
+            data=norm.rvs(loc=X.dot(beta.T), scale=sigma2),
+            index=X.index
+        )
+        return pred_df
 
 
 def train_bayesian_ols(clean_path, features, target):
@@ -79,5 +93,6 @@ def train_bayesian_ols(clean_path, features, target):
     ]
 
     bayesian_ols = BayesianOLS(X, y, param_groups)
-    bayesian_ols.simulate_draws()
-    print(bayesian_ols.draws)
+    bayesian_ols.simulate_draws(n_draws=100)
+    pred_df = bayesian_ols.predict(X)
+    print(pred_df)
